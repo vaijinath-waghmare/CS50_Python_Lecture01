2b3220c8841276811d353eb8926e8c4c0e56c5b0
vaijinath-waghmare <59206864+vaijinath-waghmare@users.noreply.github.com>
2019-12-29 22:23:00 +0530

    Create sequences.py

---
+++ python/sequences.py
@@ -0,0 +1,4 @@
+name = "Alice"
+coordinates = (10.0,20.0)
+names = ["Name1","Name2","Name3"]
+print(f"first char of name is {name[0]},tuple is {coordinates[0]}, names are {names[0]},{names[1]},{names[2]} ")
