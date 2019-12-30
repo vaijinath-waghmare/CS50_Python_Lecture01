a66552b022f2f2b7cc7e2abb3fd94902d3cb07d9
vaijinath-waghmare <59206864+vaijinath-waghmare@users.noreply.github.com>
2019-12-30 12:38:43 +0530

    jinja conditioning

--- flask/application.py
+++ flask/application.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask,render_template
 
 app = Flask(__name__)
@@ -12,7 +14,21 @@ app = Flask(__name__)
 #def hello(name):
 #	return f"<h1>Hello {name}!!!<h1>"
 
+#@app.route("/")
+#def index():
+#	hello = "Hello"
+#	return render_template("index.html",headline=hello)
+
 @app.route("/")
 def index():
-	hello = "Hello"
-	return render_template("index.html",headline=hello)
+	now = datetime.datetime.now()
+	new_year = now.month == 1 and now.day == 1
+	#return f"Today's date(dd/mm/yyyy):{now.day}/{now.month}/{now.year}"
+	new_year = True
+	return render_template("index.html",new_year=new_year)
+
+
+@app.route("/bye")
+def bye():
+	bye = "GoodBye!!!"
+	return render_template("index.html",headline=bye)
