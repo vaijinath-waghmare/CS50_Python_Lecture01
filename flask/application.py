e16670c99c702bc91ee424b2e8831b59c441014e
vaijinath-waghmare <59206864+vaijinath-waghmare@users.noreply.github.com>
2019-12-30 11:59:04 +0530

    aaaaa

--- flask/application.py
+++ flask/application.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+
+#@app.route("/")
+#def index():
+#	return "Hello World"
+
+
+#@app.route("/<string:name>")
+#def hello(name):
+#	return f"<h1>Hello {name}!!!<h1>"
+
 @app.route("/")
 def index():
-	return "Hello World"
-
-@app.route("/<string:name>")
-def hello(name):
-	return f"Hello {name}!!!"
+	return render_template("index.html")
