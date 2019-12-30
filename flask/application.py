b8a1f564e78ecf70578778cdd445d53de97791e6
vaijinath-waghmare <59206864+vaijinath-waghmare@users.noreply.github.com>
2019-12-30 11:39:11 +0530

    a

--- flask/application.py
+++ flask/application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return "Hello World"
-	
-@app.route("/david")
-def david():
-	return "Hello David"
+
+@app.route("/<string:name>")
+def hello(name):
+	return f"Hello {name}!!!"
